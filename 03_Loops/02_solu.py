250b5181de8722924055b634fbcd4dc7cfd2758e
AKASH-PRASAD7 <akash.csel016@bppimt.ac.in>
2024-04-11 15:16:09 +0530

    loops

---
+++ 03_Loops/02_solu.py
@@ -0,0 +1,9 @@
+n=4
+
+sum=0
+for num in range(1,n+1):
+    if n%2==0:
+        sum+=num
+
+print(sum)
+
---
+++ 03_Loops/06_solu.py
@@ -0,0 +1,13 @@
+
+n=4
+
+factorial=1
+
+if(n==1 or n==0):
+    print("1")
+
+while(n>1):
+    factorial=n*factorial
+    n-=1
+
+print(factorial)
